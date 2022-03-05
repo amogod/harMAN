955190ff0c18a211d7f853d729bfba72b8a04cc0
amogod <105355730+amogod@users.noreply.github.com>
2022-03-05 17:57:29 +0000

    Change yast-builtools/devtools dep version

--- resources/gen-changelog.js
+++ resources/gen-changelog.js
@@ -3,4 +3,7 @@
 const util = require('util');
 const https = require('https');
 
-// eslint-disable-next-
+// eslint-disable-next-line import/extensions
+const packageJSON = require('../package.json');
+
+con
