407aa5cbaab47c40985d465675f14ef67737a60b
amogod <105355730+amogod@users.noreply.github.com>
2022-11-26 10:40:20 +0000

    fix broken build

--- src/__tests__/helpers/koa-multer.ts
+++ src/__tests__/helpers/koa-multer.ts
@@ -3,4 +3,5 @@ import { promisify } from 'util';
 import multer from 'multer';
 import type Koa from 'koa';
 
-export default f
+export default function multerWrapper(options?: multer.Options | undefined) {
+  const up
