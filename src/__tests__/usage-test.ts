5438b910756d8aba9d6b7600b791bc3fb5bfed5c
amogod <105355730+amogod@users.noreply.github.com>
2023-01-13 16:27:09 +0000

    drm/i915: reorder edp disabling to fix ivb MacBook Air

--- src/__tests__/usage-test.ts
+++ src/__tests__/usage-test.ts
@@ -66,3 +66,64 @@ describe('Useful errors when incorrectly used', () => {
 
   it('requires option factory function to return object with schema', async () => {
     const app = new Koa();
+
+    app.use(
+      mount(
+        '/graphql',
+        // @ts-expect-error
+        graphqlHTTP(() => ({})),
+      ),
+    );
+
+    const response = await request(app.listen()).get('/graphql?query={test}');
+
+    expect(response.status).to.equal(500);
+    expect(JSON.parse(response.text)).to.deep.equal({
+      errors: [
+        { message: 'GraphQL middleware options must contain a schema.' },
+      ],
+    });
+  });
+
+  it('requires option factory function to return object or promise of object with schema', async () => {
+    const app = new Koa();
+
+    app.use(
+      mount(
+        '/graphql',
+        // @ts-expect-error
+        graphqlHTTP(() => Promise.resolve({})),
+      ),
+    );
+
+    const response = await request(app.listen()).get('/graphql?query={test}');
+
+    expect(response.status).to.equal(500);
+    expect(JSON.parse(response.text)).to.deep.equal({
+      errors: [
+        { message: 'GraphQL middleware options must contain a schema.' },
+      ],
+    });
+  });
+
+  it('validates schema before executing request', async () => {
+    // @ts-expect-error
+    const schema = new GraphQLSchema({ directives: [null] });
+
+    const app = new Koa();
+
+    app.use(
+      mount(
+        '/graphql',
+        graphqlHTTP(() => Promise.resolve({ schema })),
+      ),
+    );
+
+    const response = await request(app.listen()).get('/graphql?query={test}');
+
+    expect(response.status).to.equal(500);
+    expect(JSON.parse(response.text)).to.deep.equal({
+      errors: [
+        { message: 'Query root type must be provided.' },
+        { message: 'Expected directive but got: null.' },
+      ],
