d3e118d876ea6fb2d05fd4c5729ba1021027dd97
amogod <105355730+amogod@users.noreply.github.com>
2023-02-02 20:08:50 +0000

    documentation [ci skip]

--- src/renderGraphiQL.ts
+++ src/renderGraphiQL.ts
@@ -212,3 +212,75 @@ add "&raw" to the end of the URL within a browser.
   <script>
     // graphiql/graphiql.min.js
     ${loadFileStaticallyFromNPM('graphiql/graphiql.min.js')}
+  </script>
+  ${subscriptionScripts}
+</head>
+<body>
+  <div id="graphiql">Loading...</div>
+  <script>
+    // Collect the URL parameters
+    var parameters = {};
+    window.location.search.substr(1).split('&').forEach(function (entry) {
+      var eq = entry.indexOf('=');
+      if (eq >= 0) {
+        parameters[decodeURIComponent(entry.slice(0, eq))] =
+          decodeURIComponent(entry.slice(eq + 1));
+      }
+    });
+    // Produce a Location query string from a parameter object.
+    function locationQuery(params) {
+      return '?' + Object.keys(params).filter(function (key) {
+        return Boolean(params[key]);
+      }).map(function (key) {
+        return encodeURIComponent(key) + '=' +
+          encodeURIComponent(params[key]);
+      }).join('&');
+    }
+    // Derive a fetch URL from the current URL, sans the GraphQL parameters.
+    var graphqlParamNames = {
+      query: true,
+      variables: true,
+      operationName: true
+    };
+    var otherParams = {};
+    for (var k in parameters) {
+      if (parameters.hasOwnProperty(k) && graphqlParamNames[k] !== true) {
+        otherParams[k] = parameters[k];
+      }
+    }
+    var fetchURL = locationQuery(otherParams);
+    // Defines a GraphQL fetcher using the fetch API.
+    function graphQLFetcher(graphQLParams, opts) {
+      return fetch(fetchURL, {
+        method: 'post',
+        headers: Object.assign(
+          {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          opts && opts.headers,
+        ),
+        body: JSON.stringify(graphQLParams),
+        credentials: 'include',
+      }).then(function (response) {
+        return response.json();
+      });
+    }
+
+    function makeFetcher() {
+      if('${typeof subscriptionEndpoint}' == 'string') {
+        let client = null;
+        let url = window.location.href;
+        if('${typeof websocketClient}' == 'string' && '${websocketClient}' === 'v1') {
+          client = window.graphqlWs.createClient({url: ${safeSerialize(
+            subscriptionEndpoint,
+          )} });
+          return window.GraphiQL.createFetcher({url, wsClient: client});
+        } else {
+          let clientClass = window.SubscriptionsTransportWs.SubscriptionClient;
+          client = new clientClass(${safeSerialize(subscriptionEndpoint)}, {
+            reconnect: true
+          });
+          return window.GraphiQL.createFetcher({url, legacyClient: client});
+        }
+      } else {
